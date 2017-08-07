31f9c24c57faa0c748a75a46d8b1668a04bb4701
Jerry W <jerrywiltse@gmail.com>
2017-08-06 20:55:38 -0400

    successfully built and packaged via generator

--- conanfile.py
+++ conanfile.py
@@ -3,13 +3,13 @@ from conans import ConanFile, tools, os
 class BoostProgram_OptionsConan(ConanFile):
     name = "Boost.Program_Options"
     version = "1.64.0"
-    generators = "txt" 
+    generators = "boost" 
     settings = "os", "arch", "compiler", "build_type"
     url = "https://github.com/boostorg/program_options"
     description = "Please visit http://www.boost.org/doc/libs/1_64_0/libs/libraries.htm"
     license = "www.boost.org/users/license.html"
     lib_short_name = "program_options"
-    build_requires = "Boost.Build/1.64.0@bincrafters/testing" 
+    build_requires = "Boost.Generator/0.0.1@bincrafters/testing" 
     requires =  "Boost.Any/1.64.0@bincrafters/testing", \
                       "Boost.Config/1.64.0@bincrafters/testing", \
                       "Boost.Core/1.64.0@bincrafters/testing", \
@@ -20,6 +20,7 @@ class BoostProgram_OptionsConan(ConanFile):
                       "Boost.Smart_Ptr/1.64.0@bincrafters/testing", \
                       "Boost.Static_Assert/1.64.0@bincrafters/testing", \
                       "Boost.Throw_Exception/1.64.0@bincrafters/testing", \
+                      "Boost.Tokenizer/1.64.0@bincrafters/testing", \
                       "Boost.Type_Traits/1.64.0@bincrafters/testing"
 
                       #any6 config0 core2 detail5 function5 iterator5 lexical_cast8 smart_ptr4 static_assert1 throw_exception2 type_traits3
@@ -28,7 +29,27 @@ class BoostProgram_OptionsConan(ConanFile):
         self.run("git clone --depth=50 --branch=boost-{0} {1}.git"
                  .format(self.version, self.url))
 
+    def build(self):
+        boost_build = self.deps_cpp_info["Boost.Build"]
+        b2_bin_name = "b2.exe" if self.settings.os == "Windows" else "b2"
+        b2_bin_dir_name = boost_build.bindirs[0]
+        b2_full_path = os.path.join(boost_build.rootpath, b2_bin_dir_name, b2_bin_name)
+
+        toolsets = {
+          'gcc': 'gcc',
+          'Visual Studio': 'msvc',
+          'clang': 'clang',
+          'apple-clang': 'darwin'}
+
+        b2_toolset = toolsets[str(self.settings.compiler)]
+        
+        self.run(b2_full_path + " -j4 -a --hash=yes toolset=" + b2_toolset)
+        
     def package(self):
         include_dir = os.path.join(self.build_folder, self.lib_short_name, "include")
         self.copy(pattern="*", dst="include", src=include_dir)
+        lib_dir = os.path.join(self.build_folder, "stage/lib")
+        self.copy(pattern="*", dst="lib", src=lib_dir)
 
+    def package_info(self):
+        self.cpp_info.libs = ["boost_%s"%(self.lib_short_name)]
