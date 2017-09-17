e78bc2fd8eac161c8c87d9efb25a6db1b011f18f
Jerry Wiltse <jerrywiltse@gmail.com>
2017-09-17 11:29:43 -0400

    Updated version to 1.65.1

--- conanfile.py
+++ conanfile.py
@@ -2,29 +2,29 @@ from conans import ConanFile, tools, os
 
 class BoostProgram_OptionsConan(ConanFile):
     name = "Boost.Program_Options"
-    version = "1.64.0"
+    version = "1.65.1"
     generators = "boost" 
     settings = "os", "arch", "compiler", "build_type"
     short_paths = True
     url = "https://github.com/bincrafters/conan-boost-program_options"
-    description = "Please visit http://www.boost.org/doc/libs/1_64_0/libs/libraries.htm"
+    description = "Please visit http://www.boost.org/doc/libs/1_65_1/libs/libraries.htm"
     license = "www.boost.org/users/license.html"
     lib_short_names = ["program_options"]
     options = {"shared": [True, False]}
     default_options = "shared=False"
     build_requires = "Boost.Generator/0.0.1@bincrafters/testing" 
-    requires =  "Boost.Any/1.64.0@bincrafters/testing", \
-                      "Boost.Config/1.64.0@bincrafters/testing", \
-                      "Boost.Core/1.64.0@bincrafters/testing", \
-                      "Boost.Detail/1.64.0@bincrafters/testing", \
-                      "Boost.Function/1.64.0@bincrafters/testing", \
-                      "Boost.Iterator/1.64.0@bincrafters/testing", \
-                      "Boost.Lexical_Cast/1.64.0@bincrafters/testing", \
-                      "Boost.Smart_Ptr/1.64.0@bincrafters/testing", \
-                      "Boost.Static_Assert/1.64.0@bincrafters/testing", \
-                      "Boost.Throw_Exception/1.64.0@bincrafters/testing", \
-                      "Boost.Tokenizer/1.64.0@bincrafters/testing", \
-                      "Boost.Type_Traits/1.64.0@bincrafters/testing"
+    requires =  "Boost.Any/1.65.1@bincrafters/testing", \
+                      "Boost.Config/1.65.1@bincrafters/testing", \
+                      "Boost.Core/1.65.1@bincrafters/testing", \
+                      "Boost.Detail/1.65.1@bincrafters/testing", \
+                      "Boost.Function/1.65.1@bincrafters/testing", \
+                      "Boost.Iterator/1.65.1@bincrafters/testing", \
+                      "Boost.Lexical_Cast/1.65.1@bincrafters/testing", \
+                      "Boost.Smart_Ptr/1.65.1@bincrafters/testing", \
+                      "Boost.Static_Assert/1.65.1@bincrafters/testing", \
+                      "Boost.Throw_Exception/1.65.1@bincrafters/testing", \
+                      "Boost.Tokenizer/1.65.1@bincrafters/testing", \
+                      "Boost.Type_Traits/1.65.1@bincrafters/testing"
 
                       #any6 config0 core2 detail5 function5 iterator5 lexical_cast8 smart_ptr4 static_assert1 throw_exception2 type_traits3
 
